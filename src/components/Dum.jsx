e01b2a2c6dff4befb2db05cd4429279745d87c67
Maarij <mniazi.bese20seecs@seecs.edu.pk>
2023-04-05 22:30:03 +0500

    navbar created

---
+++ src/components/Dum.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+    <div>
+      re voluptatibus deleniti facere, impedit pariatur mollitia, enim quas quae quis tenetur nobis nulla iure eveniet nam veniam? Id saepe doloremque delectus at, iste ipsum dolorum nam ullam sit inventore cumque, veniam blanditiis perspiciatis aliquid laborum vitae placeat rem. Unde assumenda veritatis, voluptatum temporibus, asperiores corrupti optio nemo ratione totam voluptates beatae numquam minus animi molestiae, doloremque repellat.
+    </div>
+  )
+}
+
+export default Dummy
---
+++ src/components/Navbarr.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+import profile from "../assets/profile.jpg";
+import { User } from "./style";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { MyNavbar, MyContainer, UserNavLink } from "./style";
+const Navbarr = () => {
+  return (
+      <MyNavbar expand="lg" variant="dark">
+        <MyContainer>
+        <LinkContainer to="/home">
+          <Navbar.Brand>Walkers Ireland</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/home">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/destinations">
+              <Nav.Link>Destinations</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/adventures">
+              <Nav.Link>Adventures</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/healthyeating">
+              <Nav.Link>HealthyEating</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/wellbeing">
+              <Nav.Link>Wellbeing</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/blog">
+              <Nav.Link>Blog</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/favourite">
+              <Nav.Link>Favourite</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contactus">
+              <Nav.Link>Contact Us</Nav.Link>
+            </LinkContainer>
+
+            <User>
+            <img src={profile} alt="" />
+            <UserNavLink>User</UserNavLink>
+            </User>
+          </Nav>
+        </Navbar.Collapse>
+        </MyContainer>
+      </MyNavbar>
+  );
+};
+
+export default Navbarr;
