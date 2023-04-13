c99aad51a96b30f03f1306748fdd5eee9de77862
Anas Khan <anasatwork01.gmail.com>
2023-04-13 08:55:59 +0500

    master

--- src/views/Recipes/AllRecipes.jsx
+++ src/views/Recipes/AllRecipes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecipesHeader from "../../components/RecipesHeader/RecipesHeader";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,50 +9,25 @@ import meal1 from "../../assets/meal1.jpg";
 import meal2 from "../../assets/meal2.jpg";
 import { useNavigate } from "react-router-dom";
 import {OneCard, CardImg } from "./style";
+import axios from "axios";
 
 const AllRecipes = () => {
-  const recipes = [
-    {
-      id: 1,
-      title: "Pasta",
-      ingredients: ["pasta", "tomatoes", "cheese"],
-      image:  meal1 ,
-      content: "lorem20ijq;jd jq jdlnd qlnlqd lnlqnlqljelnqdd,nqdl",
-      calories: "100",
-    },
-    {
-      id: 2,
-      title: "Pizza",
-      ingredients: ["dough", "tomatoes", "cheese"],
-      image:  meal2 ,
-      content: "lorem20ijq;jd jq jdlnd qlnlqd lnlqnlqljelnqdd,nqdl",
-      calories: "100",
-    },
-    {
-      id: 3,
-      title: "Salad",
-      ingredients: ["lettuce", "tomatoes", "cucumber"],
-      image: meal ,
-      content: "lorem20ijq;jd jq jdlnd qlnlqd lnlqnlqljelnqdd,nqdl",
-      calories: "100",
-    },
-    {
-      id: 4,
-        title: "Pizza",
-        ingredients: ["dough", "tomatoes", "cheese"],
-        image:  meal2 ,
-        content: "lorem20qdnql ndllqndlnql nqhqdqdkqdnbkqd",
-        calories: "100",
-      },
-      {
-        id: 5,
-        title: "Salad",
-        ingredients: ["lettuce", "tomatoes", "cucumber"],
-        image: meal ,
-        content: "lorem20ijq;jd jq jdlnd qlnlqd lnlqnlqljelnqdd,nqdl",
-        calories: "100",
-      },
-  ];
+  const [allMeals, setAllMeals] = useState([]);
+
+  useEffect(() => {
+    async function getAllMeals() {
+      try {
+        const res = await axios.get("http://localhost:8800/api/v1/meals");
+        setAllMeals(res.data);
+        return res;
+      } catch (err) {
+        alert(err);
+      }
+    }
+    getAllMeals();
+  }, []);
+
+  
 
   const navigate = useNavigate();
   return (
@@ -61,19 +36,19 @@ const AllRecipes = () => {
 
       <Container>
         <Row>
-          {recipes.map((recipe, index) => (
+          {allMeals.map((meal, index) => (
             <Col xs={12} md={6} lg={4} onClick={ ()=>{
-              navigate(`/healthyeating/${recipe.id}`)
+              navigate(`/healthyeating/${meal._id}`)
             }}>
               <OneCard key={index}>
-                <CardImg variant="top" src={recipe.image} />
+                <CardImg variant="top" src={`http://localhost:8800/${meal.photo}`} />
                 <Card.Body>
-                  <Card.Title>{recipe.title}</Card.Title>
-                  <Card.Text>{recipe.content}</Card.Text>
+                  <Card.Title>{meal.name}</Card.Title>
+                  <Card.Text>{`${meal.calories} calaries per serving`}</Card.Text>
                 </Card.Body>
                 <Card.Footer>
                   <small className="text-muted">
-                    Calories: {recipe.calories}
+                     {meal.kind}
                   </small>
                 </Card.Footer>
               </OneCard>
--- src/views/SinglePageRecipe/SinglePageRecipe.jsx
+++ src/views/SinglePageRecipe/SinglePageRecipe.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Title,
   Image,
@@ -11,56 +11,61 @@ import {
   Heading,
 } from "./style";
 import test from "../../assets/test.jpg";
+import { useParams } from "react-router";
+import axios from "axios";
 
 const SinglePageRecipe = () => {
+  const [meal, setMeal] = useState({
+    name:"",
+    photo:"",
+    calories:"",
+    ingredients:[],
+    recipe:"",
+    benefits:"",
+    kind:""
+  });
+
+  const {id} = useParams();
+
+  console.log(id);
+
+  useEffect(() => {
+    async function getMeal() {
+      try {
+        console.log("in function")
+        const res = await axios.get(`http://localhost:8800/api/v1/meals/${id}`);
+        setMeal(res.data);
+        return res;
+      } catch (err) {
+        alert(err);
+      }
+    }
+    getMeal();
+  }, [id]);
   return (
     <BlogContainer>
-      <Title>Recipe Name</Title>
-      <Calories>Calories: 100</Calories>
+      <Title>{meal.name}</Title>
+      <Calories>{meal.calories} Calories per servings</Calories>
       <div>
-        <Image src={test} alt="" />
+        <Image src={`http://localhost:8800/${meal.photo}`} alt="" />
       </div>
       <Heading>Ingredients</Heading>
       <Ingredient>
         <ul>
-          <li>Onion</li>
-          <li>Onion</li>
-          <li>Onion</li>
+          {
+            meal.ingredients.map((ingridient)=><li>{ingridient}</li>)
+          }
         </ul>
       </Ingredient>
 
       <Heading>Recipe</Heading>
       <Content>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nulla
-        possimus esse, nihil dignissimosandae porro minus ad, aspernatur
-        voluptate dicta, nesciunt perspiciatis incidunt facilis, praesentium
-        consectetur totam accusamus maiores corrupti iste vitae assumenda iure.
-        Tempore adipisci a ipsam ratione quidem voluptate, fionem quam et, iure
-        iste facilis, eum eaque cum laudantium ut doloremque, suscipit illo
-        eligendi sed. Voluptatem est vel quo reprehenderit vitae expedita harum
-        voluptas, quasi aut repudiandae officiis nam sequi unde obcaecati iure
-        omnis rerum distinctio repellat voluptatibus doloribus corporis placeat
-        minima? Debitis delectus asperiores repudiandae officiis facere
-        perferendis dicta autem laudantium magnam accusantium? Harum culpa,
-        alias commodi maxime ex, esse, reprehenderit et earum dolor molestias
-        dolores? Vero nobis eum et voluptatem autem quos corporis dolore
-        molestias laudantium provident, veniam nihil labore dicta expedita eos?
+        {meal.recipe}
       </Content>
 
       <Heading>Benefits</Heading>
       <Content>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi debitis
-        at a exercitationem, dolorem modi, iusto iure harum eum fugiat id nobis
-        vero, laborum corporis dolorum nam in assumenda? Doloremque! Lorem ipsum
-        dolor sit amet consectetur, adipisicing elit. Nisi debitis at a
-        exercitationem, dolorem modi, iusto iure harum eum fugiat id nobis vero,
-        laborum corporis dolorum nam in assumenda? Doloremque! Lorem ipsum dolor
-        sit amet consectetur, adipisicing elit. Nisi debitis at a
-        exercitationem, dolorem modi, iusto iure harum eum fugiat id nobis vero,
-        laborum corporis dolorum nam in assumenda? Doloremque! Lorem ipsum dolor
-        sit amet consectetur, adipisicing elit. Nisi debitis at a
-        exercitationem, dolorem modi, iusto iure harum eum fugiat id nobis vero,
-        laborum corporis dolorum nam in assumenda? Doloremque!
+        {meal.benefits}
       </Content>
     </BlogContainer>
   );
